f0f4e6eb3b9608bc70e3d2489379b0acbefd7dee
iyn000 <iyn000@naver.com>
2023-04-10 17:33:30 +0900

    마이페이지 리스트(쿠폰,적립금,후기)

--- gamedori/src/main/java/game/dori/dao/ReviewDAO.java
+++ gamedori/src/main/java/game/dori/dao/ReviewDAO.java
@@ -1,12 +1,29 @@
 package game.dori.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import game.dori.vo.REVIEW_VO;
 
 @Repository
 public class ReviewDAO {
 
 	@Autowired
 	private SqlSession sqlSession;
+	
+	//마이페이지 리뷰 리스트 출력
+		public List<REVIEW_VO> selectList3(int member_idx) {
+			return sqlSession.selectList("game.dori.mapper.reviewMapper.selectList3", member_idx);
+		}
+
+		
+	//마이페이지 리뷰 개수
+	public int selectListCount2(int member_td_idx){
+		
+		return sqlSession.selectOne("game.dori.mapper.reviewMapper.selectListCount2", member_td_idx);
+	}
+		
 }
---
+++ gamedori/src/main/java/game/dori/vo/COUPON_VO.java
@@ -0,0 +1,80 @@
+package game.dori.vo;
+
+//쿠폰 VO
+public class COUPON_VO {
+	
+	private int		coupon_idx; 		//쿠폰 인덱스
+	private int 	member_tb_idx; 		//회원 인덱스
+	private String 	coupon_name; 		//쿠폰 이름
+	private double 	coupon_discount; 	//할인율
+	private String 	coupon_date; 		//쿠폰 생성일
+	private String 	coupon_start;		//쿠폰 시작일
+	private String 	coupon_end; 		//쿠폰 종료일
+	private int 	coupon_min_amount; 	//최소주문금액
+	private int 	coupon_max_amount; 	//최대할인금액
+	
+	
+	
+	
+	
+	//게터 세터
+	public int getCoupon_idx() {
+		return coupon_idx;
+	}
+	public void setCoupon_idx(int coupon_idx) {
+		this.coupon_idx = coupon_idx;
+	}
+	public int getMember_tb_idx() {
+		return member_tb_idx;
+	}
+	public void setMember_tb_idx(int member_tb_idx) {
+		this.member_tb_idx = member_tb_idx;
+	}
+	public String getCoupon_name() {
+		return coupon_name;
+	}
+	public void setCoupon_name(String coupon_name) {
+		this.coupon_name = coupon_name;
+	}
+	public double getCoupon_discount() {
+		return coupon_discount;
+	}
+	public void setCoupon_discount(double coupon_discount) {
+		this.coupon_discount = coupon_discount;
+	}
+	public String getCoupon_date() {
+		return coupon_date;
+	}
+	public void setCoupon_date(String coupon_date) {
+		this.coupon_date = coupon_date;
+	}
+	public String getCoupon_start() {
+		return coupon_start;
+	}
+	public void setCoupon_start(String coupon_start) {
+		this.coupon_start = coupon_start;
+	}
+	public String getCoupon_end() {
+		return coupon_end;
+	}
+	public void setCoupon_end(String coupon_end) {
+		this.coupon_end = coupon_end;
+	}
+	public int getCoupon_min_amount() {
+		return coupon_min_amount;
+	}
+	public void setCoupon_min_amount(int coupon_min_amount) {
+		this.coupon_min_amount = coupon_min_amount;
+	}
+	public int getCoupon_max_amount() {
+		return coupon_max_amount;
+	}
+	public void setCoupon_max_amount(int coupon_max_amount) {
+		this.coupon_max_amount = coupon_max_amount;
+	}
+
+	
+
+	
+	
+}
