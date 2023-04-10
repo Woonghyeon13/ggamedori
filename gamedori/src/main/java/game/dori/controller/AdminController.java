0da27479bd5493b2412d6d083f74145b24b44578
Woonghyeon13 <127084730+Woonghyeon13@users.noreply.github.com>
2023-04-10 09:02:20 +0900

    상품 수정1

--- gamedori/src/main/java/game/dori/controller/AdminController.java
+++ gamedori/src/main/java/game/dori/controller/AdminController.java
@@ -2,13 +2,18 @@ package game.dori.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import game.dori.service.ProductService;
@@ -31,15 +36,19 @@ public class AdminController {
 
 	// 상품관리
 	@RequestMapping( value = "/prod.do", method = RequestMethod.GET )
-	public String prod( Model model ){
+	public String prod( Model model, PRODUCT_VO pvo ){
 		List<CATEGORY_VO> category = null;
 		category = productService.category();
 		model.addAttribute("category", JSONArray.fromObject(category));
+		
+		List<PRODUCT_VO> list = productService.list(pvo);
+		model.addAttribute("plist",list);
+		
 		return "admin/prod";
 	}
 	// 상품등록
 	@RequestMapping( value = "/prod.do", method = RequestMethod.POST )
-	public String prod( PRODUCT_VO pvo, MultipartFile prod_file1, MultipartFile prod_file2, MultipartFile prod_file3 ) throws IllegalStateException, IOException{
+	public void prod( PRODUCT_VO pvo, HttpServletRequest req, HttpServletResponse rsp ,MultipartFile prod_file1, MultipartFile prod_file2, MultipartFile prod_file3 ) throws IllegalStateException, IOException{
 		
 		String path = "C:\\Users\\720\\git\\ggamedori\\gamedori\\src\\main\\webapp\\resources\\images";
 		
@@ -66,16 +75,74 @@ public class AdminController {
 			prod_file3.transferTo(new File(path,newFileName));
 			
 		}
-		String pfile1 = prod_file1.getOriginalFilename();
-		String pfile2 = prod_file2.getOriginalFilename();
-		String pfile3 = prod_file3.getOriginalFilename();
-		pvo.setProd_imgt(pfile1);
-		pvo.setProd_imgm(pfile2);
-		pvo.setProd_imgd(pfile3);
+		pvo.setProd_imgt(prod_file1.getOriginalFilename());
+		pvo.setProd_imgm(prod_file2.getOriginalFilename());
+		pvo.setProd_imgd(prod_file3.getOriginalFilename());
 		
-		productService.prodInsert(pvo);
-		return "admin/prod";
+		int result = productService.prodInsert(pvo);
+		
+		rsp.setContentType("text/html;charset=utf-8");
+		PrintWriter pw = rsp.getWriter();
+		//상품등록 성공
+		if( result > 0 )
+		{
+			pw.append("<script>alert('등록 완료');location.href='prod.do'</script>");
+		}else
+		{
+			pw.append("<script>alert('등록 실패');location.href='prod.do'</script>");
+		}
+		pw.flush();
 	}
+	
+
+	// 상품수정
+	@RequestMapping( value = "/prodmodify.do", method = RequestMethod.POST )
+	public void prodmodify( PRODUCT_VO pvo, Model model, HttpServletRequest req, HttpServletResponse rsp ,MultipartFile prod_file1, MultipartFile prod_file2, MultipartFile prod_file3 ) throws IllegalStateException, IOException{
+		
+		String path = "C:\\Users\\720\\git\\ggamedori\\gamedori\\src\\main\\webapp\\resources\\images";
+		
+		File dir = new File(path);
+		if(!dir.exists()) { 
+			dir.mkdirs();
+		}
+		String newFileName = "";
+		if(!prod_file1.getOriginalFilename().isEmpty()) { 
+			String FileName = System.currentTimeMillis()+prod_file1.getOriginalFilename();
+			newFileName = new String(FileName.getBytes("UTF-8"),"8859_1");
+			prod_file1.transferTo(new File(path,newFileName));
+			
+		}else
+			if(!prod_file2.getOriginalFilename().isEmpty()) { 
+				String FileName = System.currentTimeMillis()+prod_file2.getOriginalFilename();
+				newFileName = new String(FileName.getBytes("UTF-8"),"8859_1");
+				prod_file2.transferTo(new File(path,newFileName));
+				
+			}else
+				if(!prod_file3.getOriginalFilename().isEmpty()) { 
+					String FileName = System.currentTimeMillis()+prod_file3.getOriginalFilename();
+					newFileName = new String(FileName.getBytes("UTF-8"),"8859_1");
+					prod_file3.transferTo(new File(path,newFileName));
+					
+				}
+		pvo.setProd_imgt(prod_file1.getOriginalFilename());
+		pvo.setProd_imgm(prod_file2.getOriginalFilename());
+		pvo.setProd_imgd(prod_file3.getOriginalFilename());
+		
+		int result = productService.prodUpdate(pvo);
+		
+		rsp.setContentType("text/html;charset=utf-8");
+		PrintWriter pw = rsp.getWriter();
+		//상품등록 성공
+		if( result > 0 )
+		{
+			pw.append("<script>alert('수정 완료');location.href='prod.do'</script>");
+		}else
+		{
+			pw.append("<script>alert('수정 실패');location.href='prod.do'</script>");
+		}
+		pw.flush();
+	}
+	
 	
 
 	// 반품관리
--- gamedori/src/main/java/game/dori/service/ProductService.java
+++ gamedori/src/main/java/game/dori/service/ProductService.java
@@ -7,7 +7,14 @@ import game.dori.vo.PRODUCT_VO;
 
 public interface ProductService {
 
+	// 카테고리 리스트
 	public List<CATEGORY_VO> category();
-	
-	public void prodInsert( PRODUCT_VO pvo );
+	// 상품등록
+	public int prodInsert( PRODUCT_VO pvo );
+	// 상품목록
+	public List<PRODUCT_VO> list( PRODUCT_VO pvo );
+	// 상품수정
+	public int prodUpdate( PRODUCT_VO pvo );
+	// 상품상세
+	public PRODUCT_VO prodSelectOne( int prod_idx );
 }
