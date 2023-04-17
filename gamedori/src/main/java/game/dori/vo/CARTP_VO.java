e9db1f51f6d56b1c6cf63c68a1c410ef4f2b37dd
hanokgyeong <yuhanbilove@naver.com>
2023-04-17 09:23:51 +0900

    # WARNING: head commit changed in the meantime
    VO추가하세용~

---
+++ gamedori/src/main/java/game/dori/vo/CARTP_VO.java
@@ -0,0 +1,23 @@
+package game.dori.vo;
+
+public class CARTP_VO {
+	
+    private CART_VO cartVO;
+    private PRODUCT_VO productVO;
+    
+    
+	public CART_VO getCartVO() {
+		return cartVO;
+	}
+	public void setCartVO(CART_VO cartVO) {
+		this.cartVO = cartVO;
+	}
+	public PRODUCT_VO getProductVO() {
+		return productVO;
+	}
+	public void setProductVO(PRODUCT_VO productVO) {
+		this.productVO = productVO;
+	}
+
+    
+}
---
+++ gamedori/src/main/java/game/dori/vo/PRODUCTQQ_VO.java
@@ -0,0 +1,21 @@
+package game.dori.vo;
+
+public class PRODUCTQQ_VO {
+	
+	private PRODUCT_Q_VO product_q_vo;
+	private PRODUCT_VO product_vo;
+	
+	public PRODUCT_Q_VO getProduct_q_vo() {
+		return product_q_vo;
+	}
+	public void setProduct_q_vo(PRODUCT_Q_VO product_q_vo) {
+		this.product_q_vo = product_q_vo;
+	}
+	public PRODUCT_VO getProduct_vo() {
+		return product_vo;
+	}
+	public void setProduct_vo(PRODUCT_VO product_vo) {
+		this.product_vo = product_vo;
+	}
+	
+}
